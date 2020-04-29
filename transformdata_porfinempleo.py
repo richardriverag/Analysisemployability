3c41d2eb24c59cd884423999c05c600619ee005b
BryanV1 <bryan.vergara@epn.edu.ec>
2020-04-28 19:37:45 -0500

    limpiezadatos_porfinempleo

---
+++ transformdata_porfinempleo.py
@@ -0,0 +1,30 @@
+import datetime
+import argparse
+
+import json
+
+def main(args):
+    data={}
+    data['empleos'] = []
+    with open('data_porfinempleo_original.json',encoding='utf-8') as file:
+            informacion = json.load(file)    
+    c=0    
+    for i in informacion.values():        
+        c=c+1        
+        if i != {}:
+            data['empleos'].append(i) 
+        if c==21832:
+            break        
+
+    with open('data_porfinempleo_clean.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+if __name__=='__main__':
+    argparser = argparse.ArgumentParser(prog='Crawler template',
+        description='''Collects info from jobs portals.''')
+    
+    argparser.add_argument('-f',
+        help='file with URLS')
+    args = argparser.parse_args()
+    main(args)
+
